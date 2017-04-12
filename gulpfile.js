3fca191d96524db5d578a32a502d2224bfef4bc5
Meathill <meathill@gmail.com>
2017-04-12 14:57:48 +0800

    直接先把 Promise 的 CSS 拿来用
    Signed-off-by: Meathill <meathill@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/**
+ * Created by realm on 2017/4/12.
+ */
+
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const clean = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const sourcemap = require('gulp-sourcemaps');
+const del = require('del');
+
+const DEST = 'css/';
+
+gulp.task('del', () => {
+  return del(DEST);
+});
+
+gulp.task('css', () => {
+  return gulp.src('styl/screen.styl')
+    .pipe(stylus())
+    .pipe(rename('meathill-reveal-theme.css'))
+    .pipe(sourcemap.init())
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest(DEST))
+    .pipe(clean())
+    .pipe(rename('meathill-reveal-theme.min.css'))
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('default', ['css']);
